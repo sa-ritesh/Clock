bf737fc13d747a3f40b33beefa562d5c2abc76a6
Ritesh Arora <67755113+sa-ritesh@users.noreply.github.com>
2020-09-06 19:21:03 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,13 +1,13 @@
+var audio = new Audio("assets/tick.mp3");
 function second(){
-	var date = new Date();
+    var date = new Date();
     var deg=6;
     let hh= date.getHours()*30;
     let mm=date.getMinutes()*deg;
     let sec= date.getSeconds()*deg;
+     audio.play();
 	$("#second").css("transform", 'rotate('+sec+ 'deg)');
 	$("#minute").css("transform", 'rotate('+mm+ 'deg)');
 	$("#hour").css("transform", 'rotate('+hh+ 'deg)');
-
 }
 setInterval(second,1000);
-
